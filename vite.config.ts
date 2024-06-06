a6016536a9950d8c29de42d016aaca07df9403a6
kychongdev <kychongdev@gmail.com>
2024-06-06 23:50:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tkfm-battle/",
+  base: "/tkfm-battle",
   plugins: [react(), TanStackRouterVite()],
 });
