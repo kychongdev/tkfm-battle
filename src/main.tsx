b794203028a8e669c3cf1d77b399bdf86f12a3ec
kychongdev <kychongdev@gmail.com>
2024-06-06 23:46:53 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createMemoryHistory,
+  createRouter,
+} from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const memoryHistory = createMemoryHistory({
+  initialEntries: ["/"], // Pass your initial url
+});
+
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: memoryHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -19,6 +27,7 @@ declare module "@tanstack/react-router" {
 }
 
 // Render the app
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
