63f32e705a0168565df7f3382feb1441f47e55ab
kychongdev <kychongdev@gmail.com>
2024-06-08 10:56:46 +0800

    feat: update css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import {
   RouterProvider,
   createMemoryHistory,
